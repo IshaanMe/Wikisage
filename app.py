cc525dea341bf051788516aa805a34eaebafaed0
Ishaan Mehta <165460664+IshaanMe@users.noreply.github.com>
2025-07-09 16:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("🌐 WikiSageQA: Multilingual Wikipedia Question Answering")
 # --- LOAD MODELS ---
 @st.cache_resource
 def load_models():
-    pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
+    extractive = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
     generative = pipeline("text-generation", model="gpt2")
     return extractive, generative
 
